40782f889955788d84ff2ba0b7a0a2adb76f3e12
dmouli2 <dmouli2@gmail.com>
2019-06-20 11:30:59 +0530

    solution1Completed

---
+++ js/solution1.js
@@ -0,0 +1,29 @@
+
+let book = [
+    {
+        id:1,
+        title:'The Hobbit',
+        alreadyRead: false,
+    },
+    {
+        id:2,
+        title:'The BAT',
+        alreadyRead: false,
+    },
+    {
+        id:3,
+        title:'The Peter cok',
+        alreadyRead: false,
+    },
+    {
+        id:4,
+        title:'Please Dont Buy',
+        alreadyRead: false,
+    }
+]
+
+
+
+function readBook(){
+console.log('vbj')
+}
